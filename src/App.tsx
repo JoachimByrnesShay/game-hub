748756a57e166ee9a00d0666baf28a18a4ffd540
Joachim Byrnes-Shay <56098202+JoachimByrnesShay@users.noreply.github.com>
2024-03-14 23:33:59 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import {
+  Button,
+  ButtonGroup,
+} from "@chakra-ui/react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Button colorScheme="blue">
+          Button
+        </Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
