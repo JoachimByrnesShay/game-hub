7776c7eb8730000d8c3507f693960520dd15472c
Joachim Byrnes-Shay <56098202+JoachimByrnesShay@users.noreply.github.com>
2024-03-18 00:47:01 -0400

    refactor: remove duplicated card and skeleton styles

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,6 +5,7 @@ import {
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer.";
 
 const GameGrid = () => {
   const { games, error, isLoading } =
@@ -41,15 +42,20 @@ const GameGrid = () => {
       >
         {isLoading &&
           skeletons.map((skeleton) => (
-            <GameCardSkeleton
-              key={skeleton}
-            />
+            <GameCardContainer>
+              {" "}
+              <GameCardSkeleton
+                key={skeleton}
+              />
+            </GameCardContainer>
           ))}
         {games.map((game) => (
-          <GameCard
-            key={game.id}
-            game={game}
-          ></GameCard>
+          <GameCardContainer>
+            <GameCard
+              key={game.id}
+              game={game}
+            ></GameCard>
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </>
